315c40476c2f1877dcece5c8a550da53dae9498e
Justin Liu <justinliu@maaii.com>
2015-11-13 10:47:15 +0800

    compiled with options

--- opensslmake.sh
+++ opensslmake.sh
@@ -9,6 +9,8 @@ FIPS_VERSION=2.0.5
 FIPS_INSTALL_DIR=${SCRIPT_DIR}/fips_install
 INSTALL_DIR=${SCRIPT_DIR}/openssl_install
 
+OPENSSL_OPTIONS="threads no-shared no-hw no-dso no-krb5 no-zlib no-bf no-cast no-idea no-md2 no-mdc2 no-rc5 no-ripemd no-sctp"
+
 rm -Rf openssl-fips-${FIPS_VERSION}
 rm -Rf openssl-${OPENSSL_VERSION}
 rm -Rf ${FIPS_INSTALL_DIR}
@@ -22,26 +24,23 @@ export FIPS_SIG=${SCRIPT_DIR}/openssl-fips-${FIPS_VERSION}/util/incore
 
 . ./setenv-android.sh
 pushd openssl-fips-${FIPS_VERSION}
-./config --prefix=${FIPS_INSTALL_DIR}/
+./config --prefix=${FIPS_INSTALL_DIR}/ ${OPENSSL_OPTIONS}
 make
 
 make install
 cp $FIPS_SIG ${FIPS_INSTALL_DIR}/bin
 popd
-#cp -R /usr/local/ssl/fips-2.0 fips-2.0
-#cp ${FIPS_SIG} fips-2.0/bin
 
 
 tar xzf openssl-${OPENSSL_VERSION}.tar.gz
 . ./setenv-android.sh
 pushd openssl-${OPENSSL_VERSION}
 perl -pi -e 's/install: all install_docs install_sw/install: install_sw/g' Makefile.org
-./config fips --with-fipsdir=${FIPS_INSTALL_DIR}/ --prefix=${INSTALL_DIR}/
+./config fips --with-fipsdir=${FIPS_INSTALL_DIR}/ --prefix=${INSTALL_DIR}/ ${OPENSSL_OPTIONS}
 make depend
 make all
 make install CC=$ANDROID_TOOLCHAIN/arm-linux-androideabi-gcc RANLIB=$ANDROID_TOOLCHAIN/arm-linux-androideabi-ranlib
 popd
-# cp -R /usr/local/ssl ${OPENSSL_INSTALL_DIR}
 
 
 popd
